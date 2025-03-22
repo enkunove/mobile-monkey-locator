47868deac53cbec92412a5c6461b7be1d363ea57
Egor <eshevkunovs@gmail.com>
2025-03-23 01:19:47 +0300

    First version

---
+++ monkey_finder/Model/MonkeyModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace monkey_finder.Model
+{
+    class MonkeyModel
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Details { get; set; }
+        public string Image { get; set; }
+        public int Population { get; set; }
+        public double Latitude { get; set; }
+
+        public double Longtitude { get; set; }
+    }
+}
