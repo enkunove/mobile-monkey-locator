b57b4f7547d9f0e28dcf65e1adc1a4d39b2311d0
Egor <eshevkunovs@gmail.com>
2025-03-23 21:34:06 +0300

    Final commit

--- monkey_finder/Lib/ViewModel/MonkeyDetailsViewModel.cs
+++ monkey_finder/Lib/ViewModel/MonkeyDetailsViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using monkey_finder.Lib.Model;
 using monkey_finder.ViewModel;
 using System;
@@ -9,14 +10,40 @@ using System.Threading.Tasks;
 
 namespace monkey_finder.ViewModel;
 
-    [QueryProperty("Monkey", "Monkey")]
-    public partial class MonkeyDetailsViewModel : BaseViewModel
-    {
-        [ObservableProperty]
-        MonkeyModel monkey;
+[QueryProperty("Monkey", "Monkey")]
+public partial class MonkeyDetailsViewModel : BaseViewModel
+{
+    [ObservableProperty]
+    MonkeyModel monkey;
 
-        public MonkeyDetailsViewModel()
+    private readonly IMap _map;
+
+    public MonkeyDetailsViewModel(IMap map)
+    {
+        _map = map;
+    }
+
+    [RelayCommand]
+
+    public async Task OpenMapAsync()
+    {
+        try
         {
+            await _map.OpenAsync(Monkey.Latitude, Monkey.Longtitude, new MapLaunchOptions
+            {
+                Name = Monkey.Name,
+                NavigationMode = NavigationMode.None
+            });
+        }
+        catch
+        {
+            await Shell.Current.DisplayAlert("Error", "Unable to open map", "OK");
+
+        }
+        finally
+        {
+
         }
     }
+}
 
