572d80e497d9d48669734896d7be60ce7f21b8db
Egor <eshevkunovs@gmail.com>
2025-03-23 20:05:46 +0300

    Geolocation & Permissions Implementation

--- monkey_finder/Lib/ViewModel/MonkeysViewModel.cs
+++ monkey_finder/Lib/ViewModel/MonkeysViewModel.cs
@@ -11,13 +11,53 @@ namespace monkey_finder.ViewModel;
 public partial class MonkeysViewModel : BaseViewModel
 {
     private readonly MonkeyService _monkeyService;
+    private readonly IConnectivity _connectivity;
+    private readonly IGeolocation _geolocation;
 
     public ObservableCollection<MonkeyModel> Monkeys { get; } = new();
 
-    public MonkeysViewModel(MonkeyService monkeyService)
+    public MonkeysViewModel(MonkeyService monkeyService, IConnectivity connectivity, IGeolocation geolocation)
     {
         Title = "Monkeys Finder";
         _monkeyService = monkeyService;
+        _connectivity = connectivity;
+        _geolocation = geolocation;
+    }
+
+    [RelayCommand]
+    public async Task GetClosestMonkeyAsync()
+    {
+        if (IsBusy || Monkeys.Count == 0)
+            return;
+        try 
+        {
+            var location = await _geolocation.GetLastKnownLocationAsync();
+            if (location == null)
+            {
+                location = await _geolocation.GetLocationAsync(
+                    new GeolocationRequest
+                    {
+                        DesiredAccuracy = GeolocationAccuracy.Medium,
+                        Timeout = TimeSpan.FromSeconds(30)
+                    }
+                    );
+            }
+            if (location == null)
+                return;
+            var first = Monkeys.OrderBy(m => location.CalculateDistance(m.Latitude, m.Longtitude, DistanceUnits.Kilometers)).FirstOrDefault();
+            if (first is null)
+                return;
+            await Shell.Current.DisplayAlert("Closest monkey", $"{first.Name} in {first.Location}", "OK");
+
+        }
+        catch
+        {
+            await Shell.Current.DisplayAlert("Error", "Unable to find closest monkey", "OK");
+        }
+        finally
+        {
+
+        }
     }
 
     [RelayCommand]
@@ -37,6 +77,11 @@ public partial class MonkeysViewModel : BaseViewModel
 
         try
         {
+            if (_connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                await Shell.Current.DisplayAlert("Error", "No Internet connection", "OK");
+                return;
+            }
             IsBusy = true;
 
             var monkeys = await _monkeyService.GetMonkeys();
